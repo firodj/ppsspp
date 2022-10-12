9f80b1016034e3f14c1e07478e4bca816d3714f2
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2022-10-12 13:41:08 +0700

    remoge old pspdisasm

--- disasm/BindConfig.cmake
+++ disasm/BindConfig.cmake
@@ -5,4 +5,4 @@ SET(INCLUDE_PATH "${CMAKE_ARGV4}")
 SET(LIBRARY_PATH "${CMAKE_ARGV5}")
 
 message("${LIBRARY_PATH} ${INCLUDE_PATH}")
-configure_file(${HERE}/pspdisasm/bridge/bridge.go.in ${HERE}/pspdisasm/bridge/bridge.go)
+configure_file(${HERE}/pspsora/bridge/bridge.go.in ${HERE}/pspsora/bridge/bridge.go)
